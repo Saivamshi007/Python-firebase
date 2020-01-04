d7b5d7f74f4063b20fa25aafaf9f89e6a405949f
Sunny <dsaivamshi123@gmail.com>
2020-01-05 02:14:12 +0530

    streamlit in

--- sample.py
+++ sample.py
@@ -1,12 +1,27 @@
 from firebase import firebase
+import streamlit as st
+
+
+def upload():
+
+	data={
+    	'Name':name,
+    	'Age':age
+	}
+	result=firebase.post('/pythontest-29138/New',data)
+	result2=firebase.get('/pythontest-29138/New','')
+	st.success(result2)
+
 
 firebase=firebase.FirebaseApplication("https://pythontest-29138.firebaseio.com/",None)
-name=input("Enter your name")
-age=input("Enter your age")
-data={
-    'Name':name,
-    'Age':age
-}
-result=firebase.post('/pythontest-29138/New',data)
-result2=firebase.get('/pythontest-29138/New','')
-print(result2)
+st.title("Python connectivity with firebase")
+name=st.text_input("Enter your name")
+age=st.text_input("Enter your age")
+
+
+if st.button("Upload"):
+	upload()
+
+
+
+
